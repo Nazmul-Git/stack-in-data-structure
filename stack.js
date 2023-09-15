827413510f92129e51d62733e178ff7b9a9907eb
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-16 04:21:08 +0600

    From stack peek the value of the top element.

--- stack.js
+++ stack.js
@@ -11,8 +11,32 @@ Stack is a linear data structure in which insertion and deletion are done at one
 
 // push FIFO / LIFO
 const arr=[];
-arr.push(10);
+arr.push(10);  // first in
 arr.push(20);
 arr.push(30); // last in 30
 console.log(arr);
 console.log(arr.pop(), arr); // first out 30
+
+console.log(arr); // [ 10, 20 ]
+console.log(arr.pop(), arr); // out 20 , [ 10 ]
+
+console.log(arr);
+console.log(arr.pop(), arr); // out 10, last out []
+
+// IsEmpty ?
+if(arr.length===0){
+    console.log('underflow = ',arr); 
+    console.log(arr.pop()); //// Underflow if the stack is empty.
+}
+// IsEmpty ? for string
+const str="";
+for(let i=0; i<str.length; i++){
+    console.log(str+str[i]+'');
+}
+
+// IsFull ?
+
+// top/Peek: Get the value of the top element
+const arr2=[11,22,33,44,55];
+const peek=arr2.length-1;
+console.log(arr2[peek]);  // get the top value
